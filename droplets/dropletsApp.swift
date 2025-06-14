9500ae18e8f8c5eda8e009d797f0fbe37b89e823
lareenmelo <lareenmmelo@gmail.com>
2025-06-14 12:23:29 -0400

    first commit

---
+++ droplets/dropletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropletsApp.swift
+//  droplets
+//
+//  Created by Lareen Melo on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct dropletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
