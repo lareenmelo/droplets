b90e56f18cc2d19b49a87f3fade5f8c5da3175dc
lareenmelo <lareenmmelo@gmail.com>
2025-10-14 02:16:13 -0400

    adding test target + test file

---
+++ dropletsTests/dropletsTests.swift
@@ -0,0 +1,16 @@
+//
+//  dropletsTests.swift
+//  dropletsTests
+//
+//  Created by Lareen Melo on 10/9/25.
+//
+
+import Testing
+
+struct dropletsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
